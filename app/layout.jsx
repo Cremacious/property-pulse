d7d31e62e0bf8deacea30b4d544dc27160d80343
Cremacious <108367916+Cremacious@users.noreply.github.com>
2025-01-13 12:53:13 -0500

    Installed icons and added click functionality

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,12 +4,12 @@ import NavBar from '@/components/Navbar';
 export const metadata = {
   title: 'Property Pulse',
   keywords: 'Real Estate, Property, Buy, Sell, Rent',
-    description: 'A real estate website to buy, sell, and rent properties',
+  description: 'A real estate website to buy, sell, and rent properties',
 };
 
 const MainLayout = ({ children }) => {
   return (
-    <html>
+    <html lang="en" suppressHydrationWarning>
       <body>
         <NavBar />
         <main>{children}</main>
