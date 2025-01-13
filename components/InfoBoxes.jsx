c67632b66b40ddf5ae60e9b860bd92bc335459fa
Cremacious <108367916+Cremacious@users.noreply.github.com>
2025-01-13 15:39:38 -0500

    Made buttons dynamic.

--- components/InfoBoxes.jsx
+++ components/InfoBoxes.jsx
@@ -6,11 +6,26 @@ const InfoBoxes = () => {
     <section>
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-          <InfoBox heading="For Renters">
+          <InfoBox
+            heading="For Renters"
+            buttonInfo={{
+              link: '/properties',
+              text: 'Browse Properties',
+              backgroundColor: 'bg-black',
+            }}
+          >
             Find your dream rental property. Bookmark properties and contact
             owners.
           </InfoBox>
-          <InfoBox backgroundColor="bg-blue-100" heading="For Property Owners">
+          <InfoBox
+            backgroundColor="bg-blue-100"
+            heading="For Property Owners"
+            buttonInfo={{
+              link: '/properties/add',
+              text: 'Add Properties',
+              backgroundColor: 'bg-blue-500',
+            }}
+          >
             List your properties and reach potential tenants. Rent as an airbnb
             or long term.
           </InfoBox>
