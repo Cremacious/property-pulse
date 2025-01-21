bbe5d1c61b1bff90d477a5efb9a5ea554d400d7b
Cremacious <108367916+Cremacious@users.noreply.github.com>
2025-01-21 09:33:30 -0500

    integrated cloudinary

--- app/actions/addProperty.js
+++ app/actions/addProperty.js
@@ -2,9 +2,9 @@
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
 import { getSessionUser } from '@/utils/getSessionUser';
-import { connect } from 'mongoose';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import cloudinary from '@/config/cloudinary';
 
 async function addProperty(formData) {
   await connectDB();
@@ -14,10 +14,7 @@ async function addProperty(formData) {
   }
   const { userId } = sessionUser;
   const amenities = formData.getAll('amenities');
-  const images = formData
-    .getAll('images')
-    .filter((image) => image.name !== '')
-    .map((image) => image.name);
+  const images = formData.getAll('images').filter((image) => image.name !== '');
 
   const propertyData = {
     owner: userId,
@@ -44,8 +41,26 @@ async function addProperty(formData) {
       email: formData.get('seller_info.email'),
       phone: formData.get('seller_info.phone'),
     },
-    images,
   };
+
+  const imageUrls = [];
+
+  for (const imageFile of images) {
+    const imageBuffer = await imageFile.arrayBuffer();
+    const imageArray = Array.from(new Uint8Array(imageBuffer));
+    const imageData = Buffer.from(imageArray);
+
+    const imageBase64 = imageData.toString('base64');
+
+    const result = await cloudinary.uploader.upload(
+      `data:image/jpeg;base64,${imageBase64}`,
+      { folder: 'propertypulse' }
+    );
+    imageUrls.push(result.secure_url);
+  }
+
+  propertyData.images = imageUrls;
+
   const newProperty = new Property(propertyData);
   await newProperty.save();
   revalidatePath('/', 'layout');
