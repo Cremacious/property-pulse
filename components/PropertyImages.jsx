17949bb1d53413d3120b06d447bb35982beac1ca
Cremacious <108367916+Cremacious@users.noreply.github.com>
2025-01-21 12:03:11 -0500

    property displays image on details page at bottom

---
+++ components/PropertyImages.jsx
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+
+const PropertyImages = ({ images }) => {
+  return (
+    <section className="bg-blue-50 p-4">
+      <div className="container mx-auto">
+        {images.length === 1 ? (
+          <Image
+            src={images[0]}
+            alt=""
+            className="h-[400px] object-cover mx-auto rounded-xl"
+            width={1800}
+            height={400}
+            priority={true}
+          />
+        ) : (
+          <div className="grid grid-cols-2 gap-4">
+            {images.map((image, index) => (
+              <div key={index} className="col-span-1">
+                <Image
+                  src={image}
+                  alt=""
+                  className="h-[400px] object-cover w-full rounded-xl"
+                  width={1800}
+                  height={400}
+                  priority={true}
+                />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default PropertyImages;
