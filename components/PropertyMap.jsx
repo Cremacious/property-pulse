957f5e5cf7f19c1345a18f46936502f6d22e4fb2
Cremacious <108367916+Cremacious@users.noreply.github.com>
2025-01-22 12:11:50 -0500

    Working map with pin

--- components/PropertyMap.jsx
+++ components/PropertyMap.jsx
@@ -1,9 +1,11 @@
 'use client';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import { useEffect, useState } from 'react';
 import { setDefaults, fromAddress } from 'react-geocode';
-import Map, {Marker} from 'react-map-gl';
+import Map, { Marker } from 'react-map-gl';
 import Image from 'next/image';
 import pin from '@/assets/images/pin.svg';
+import Spinner from '@/components/Spinner';
 
 const PropertyMap = ({ property }) => {
   const [lat, setLat] = useState(null);
@@ -34,7 +36,6 @@ const PropertyMap = ({ property }) => {
           return;
         }
         const { lat, lng } = res.results[0].geometry.location;
-        console.log(lat, lng);
         setLat(lat);
         setLng(lng);
         setViewport({
@@ -51,15 +52,29 @@ const PropertyMap = ({ property }) => {
     };
     fetchCoords();
   }, []);
-  if (loading) return <h3>Loading...</h3>;
+  if (loading) return <Spinner />;
   if (geocodeError) {
     return <div className="text-xl">No location data found</div>;
   }
 
   return (
-    <div>
-      <h1>Poop</h1>
-    </div>
+    !loading && (
+      <Map
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+        mapLib={import('mapbox-gl')}
+        initialViewState={{
+          longitude: lng,
+          latitude: lat,
+          zoom: 15,
+        }}
+        style={{ width: '100%', height: 500 }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      >
+        <Marker longitude={lng} latitude={lat} anchor="bottom">
+          <Image src={pin} alt="location" width={40} height={40} />
+        </Marker>
+      </Map>
+    )
   );
 };
 
