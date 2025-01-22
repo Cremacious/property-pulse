307b9ed4b21252ec686e1b1b3af7fa95c6b20e28
Cremacious <108367916+Cremacious@users.noreply.github.com>
2025-01-22 09:46:29 -0500

    can now edit property by adding correct action

--- app/actions/updateProperty.js
+++ app/actions/updateProperty.js
@@ -1,30 +1,25 @@
 'use server';
-import Property from '../models/property';
-import { connectDB } from '../config/database';
-import { convertToSerializedObject } from '../utils/convertToObject';
+
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
 import { getSessionUser } from '@/utils/getSessionUser';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
-async function updateProperty({ propertyID, formData }) {
+async function updateProperty(propertyId, formData) {
   await connectDB();
-  const sessionUser = await getSessionUser();
-  if (!sessionUser || !sessionUser.userId) {
-    throw new Error('You must be logged in to add a property');
-  }
-  const { userId } = sessionUser;
-  const existingProperty = await Property.findById(propertyID);
 
-  if (!existingProperty) {
-    throw new Error('Property not found');
-  }
+  const sessionUser = await getSessionUser();
+
+  const { userId } = sessionUser;
+
+  const existingProperty = await Property.findById(propertyId);
 
   if (existingProperty.owner.toString() !== userId) {
-    throw new Error('You do not have permission to edit this property');
+    throw new Error('Current user does not own this property.');
   }
 
   const propertyData = {
-    owner: userId,
     type: formData.get('type'),
     name: formData.get('name'),
     description: formData.get('description'),
@@ -32,28 +27,32 @@ async function updateProperty({ propertyID, formData }) {
       street: formData.get('location.street'),
       city: formData.get('location.city'),
       state: formData.get('location.state'),
-      zip: formData.get('location.zipcode'),
+      zipcode: formData.get('location.zipcode'),
     },
     beds: formData.get('beds'),
     baths: formData.get('baths'),
     square_feet: formData.get('square_feet'),
-    amenities,
+    amenities: formData.getAll('amenities'),
     rates: {
-      nightly: formData.get('rates.nightly'),
       weekly: formData.get('rates.weekly'),
       monthly: formData.get('rates.monthly'),
+      nightly: formData.get('rates.nightly.'),
     },
     seller_info: {
       name: formData.get('seller_info.name'),
       email: formData.get('seller_info.email'),
       phone: formData.get('seller_info.phone'),
     },
+    owner: userId,
   };
+
   const updatedProperty = await Property.findByIdAndUpdate(
-    propertyID,
+    propertyId,
     propertyData
   );
+
   revalidatePath('/', 'layout');
+
   redirect(`/properties/${updatedProperty._id}`);
 }
 
