0d33b7ef4ec44291305495236ed2c0162c0d4a14
Cremacious <108367916+Cremacious@users.noreply.github.com>
2025-01-22 14:16:01 -0500

    Added sidebar options.

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -7,6 +7,10 @@ import Link from 'next/link';
 import { FaArrowLeft } from 'react-icons/fa';
 import { convertToSerializedObject } from '@/utils/convertToObject';
 
+import BookmarkButton from '@/components/BookmarkButton';
+import ShareButtons from '@/components/ShareButtons';
+import PropertyContactForm from '@/components/PropertyContactForm';
+
 const PropertyPage = async ({ params }) => {
   await connectDB();
   const { id } = await params;
@@ -38,6 +42,11 @@ const PropertyPage = async ({ params }) => {
         <div className="container m-auto py-10 px-6">
           <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
             <PropertyDetails property={property} />
+            <aside className="space-y-4">
+              <BookmarkButton property={property} />
+              <ShareButtons property={property} />
+              <PropertyContactForm property={property} />
+            </aside>
           </div>
         </div>
       </section>
